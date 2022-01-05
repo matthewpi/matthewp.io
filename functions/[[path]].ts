bc12da6cafcf8676121d2ba08f3be65da79d660f
Matthew Penner <me@matthewp.io>
2022-01-04 17:54:09 -0700

    :(

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -35,52 +35,5 @@ const handleRequest = createPagesFunctionHandler({
 });
 
 export async function onRequest(context: EventContext<Env, any, any>): Promise<Response> {
-	const ifNoneMatch = context.request.headers.get('if-none-match');
-
-	return handleRequest({
-		...context,
-		env: {
-			...context.env,
-			// eslint-disable-next-line @typescript-eslint/naming-convention
-			ASSETS: {
-				...context.env.ASSETS,
-				fetch:
-					// @ts-expect-error NODE_ENV gets replaced, it shouldn't be accessed by an index signature.
-					// eslint-disable-next-line node/prefer-global/process
-					process.env.NODE_ENV === 'production'
-						? async (
-								request: Request | string,
-								requestInitr?: RequestInit | Request,
-						  ) => {
-								if (
-									typeof request !== 'string' &&
-									ifNoneMatch !== null &&
-									!request.headers.has('if-none-match')
-								) {
-									request.headers.set('if-none-match', ifNoneMatch);
-								}
-
-								let response = await context.env.ASSETS.fetch(
-									request,
-									requestInitr,
-								);
-								if (response.ok) {
-									response = new Response(
-										[101, 204, 205, 304].includes(response.status)
-											? null
-											: response.body,
-										response,
-									);
-									response.headers.set(
-										'cache-control',
-										'public, max-age=31536000, immutable',
-									);
-								}
-
-								return response;
-						  }
-						: context.env.ASSETS.fetch,
-			},
-		},
-	});
+	return handleRequest(context);
 }
